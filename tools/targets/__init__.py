0a41148e2c1b1738adebcc7e744b5248142fb549
Michael Schwarcz <Michael.Schwarcz@arm.com>
2018-12-19 15:45:46 +0200

    Add ARM_MUSCA_A1 as a new target platform
    Musca-A1 is a Cortex-M33 based target with security extension enabled.
    
    - ARM_MUSCA_A1 is the non-secure target running mbed-os.
    - ARM_MUSCA_A1_S is the secure target running TF-M.
    - TF-M sources were imported and patched in previous commits.
    - TF-M secure bootloader (McuBoot) for MUSCA_A1 is submitted by a pre-built binary.
    - A post-build hook concatenates The secure and non-secure binaries,
      signs it and then concatenates the bootloader with the signed binary.

--- tools/targets/__init__.py
+++ tools/targets/__init__.py
@@ -592,6 +592,13 @@ class PSOC6Code:
         else:
             psoc6_complete(t_self, elf, binf)
 
+class ArmMuscaA1Code:
+    """Musca-A1 Hooks"""
+    @staticmethod
+    def binary_hook(t_self, resources, elf, binf):
+        from tools.targets.ARM_MUSCA_A1.ARM_MUSCA_A1_TFM import musca_tfm_bin
+        musca_tfm_bin(t_self, binf)
+
 ################################################################################
 
 # Instantiate all public targets
